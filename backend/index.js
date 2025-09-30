1ec230db74f0216f469595dc6b4105710193cd79
Nitesh Gupta <niteshgupta0985@gmail.com>
2025-09-30 12:31:02 +0530

    Update index.js
    all origin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 // Setup Socket.io
 const io = new Server(server, {
   cors: {
-    origin: "https://chatof-help-and-supports.vercel.app", // frontend origin
+    origin: "*", // frontend origin
     methods: ["GET", "POST"]
   }
 });
