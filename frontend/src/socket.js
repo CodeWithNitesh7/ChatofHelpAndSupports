79b17b8b654acb21cc188c9800ac0b41da4a9140
Nitesh Gupta <niteshgupta0985@gmail.com>
2025-09-30 11:55:24 +0530

    Update socket.js
    adding Live Backend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import {io} from 'socket.io-client'
 
 // const socket = io('http://localhost:9000')
-const socket = io('https://k30nk11j-9000.inc1.devtunnels.ms/')
+// const socket = io('https://k30nk11j-9000.inc1.devtunnels.ms/')
+const socket = io(' https://chatofhelpandsupports.onrender.com')
 
 export default socket;
